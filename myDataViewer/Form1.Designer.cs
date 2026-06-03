ca20214fc074716f5b309172eed8543968e579df
zizwiz <zizwiz@engineer.com>
2026-06-03 15:33:14 +0100

    Initial upload of files
    Initial upload of files

---
+++ myDataViewer/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace myDataViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabTemperature = new System.Windows.Forms.TabPage();
+            this.tabHumidity = new System.Windows.Forms.TabPage();
+            this.chartTemperature = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartHumidity = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkedListSensors = new System.Windows.Forms.CheckedListBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.comboYear = new System.Windows.Forms.ComboBox();
+            this.comboMonth = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabTemperature.SuspendLayout();
+            this.tabHumidity.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTemperature)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartHumidity)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1235, 661);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1229, 455);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabTemperature);
+            this.tabControl1.Controls.Add(this.tabHumidity);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1229, 455);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabTemperature
+            // 
+            this.tabTemperature.Controls.Add(this.chartTemperature);
+            this.tabTemperature.Location = new System.Drawing.Point(4, 29);
+            this.tabTemperature.Name = "tabTemperature";
+            this.tabTemperature.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTemperature.Size = new System.Drawing.Size(1221, 562);
+            this.tabTemperature.TabIndex = 0;
+            this.tabTemperature.Text = "Temperature";
+            this.tabTemperature.UseVisualStyleBackColor = true;
+            // 
+            // tabHumidity
+            // 
+            this.tabHumidity.Controls.Add(this.chartHumidity);
+            this.tabHumidity.Location = new System.Drawing.Point(4, 29);
+            this.tabHumidity.Name = "tabHumidity";
+            this.tabHumidity.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHumidity.Size = new System.Drawing.Size(1221, 422);
+            this.tabHumidity.TabIndex = 1;
+            this.tabHumidity.Text = "Humidity";
+            this.tabHumidity.UseVisualStyleBackColor = true;
+            // 
+            // chartTemperature
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chartTemperature.ChartAreas.Add(chartArea7);
+            this.chartTemperature.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend7.Name = "Legend1";
+            this.chartTemperature.Legends.Add(legend7);
+            this.chartTemperature.Location = new System.Drawing.Point(3, 3);
+            this.chartTemperature.Name = "chartTemperature";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chartTemperature.Series.Add(series7);
+            this.chartTemperature.Size = new System.Drawing.Size(1215, 556);
+            this.chartTemperature.TabIndex = 0;
+            this.chartTemperature.Text = "chart1";
+            // 
+            // chartHumidity
+            // 
+            this.chartHumidity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea8.Name = "ChartArea1";
+            this.chartHumidity.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chartHumidity.Legends.Add(legend8);
+            this.chartHumidity.Location = new System.Drawing.Point(6, 6);
+            this.chartHumidity.Name = "chartHumidity";
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chartHumidity.Series.Add(series8);
+            this.chartHumidity.Size = new System.Drawing.Size(1209, 410);
+            this.chartHumidity.TabIndex = 0;
+            this.chartHumidity.Text = "chart1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.comboMonth);
+            this.panel2.Controls.Add(this.comboYear);
+            this.panel2.Controls.Add(this.btn_close);
+            this.panel2.Controls.Add(this.btnLoadData);
+            this.panel2.Controls.Add(this.checkedListSensors);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 464);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1229, 194);
+            this.panel2.TabIndex = 1;
+            // 
+            // checkedListSensors
+            // 
+            this.checkedListSensors.FormattingEnabled = true;
+            this.checkedListSensors.Location = new System.Drawing.Point(555, 35);
+            this.checkedListSensors.Name = "checkedListSensors";
+            this.checkedListSensors.Size = new System.Drawing.Size(386, 142);
+            this.checkedListSensors.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(1061, 35);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(158, 63);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(1061, 122);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(158, 63);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // comboYear
+            // 
+            this.comboYear.FormattingEnabled = true;
+            this.comboYear.Location = new System.Drawing.Point(32, 27);
+            this.comboYear.Name = "comboYear";
+            this.comboYear.Size = new System.Drawing.Size(121, 28);
+            this.comboYear.TabIndex = 3;
+            this.comboYear.SelectedIndexChanged += new System.EventHandler(this.comboYear_SelectedIndexChanged);
+            // 
+            // comboMonth
+            // 
+            this.comboMonth.FormattingEnabled = true;
+            this.comboMonth.Location = new System.Drawing.Point(159, 27);
+            this.comboMonth.Name = "comboMonth";
+            this.comboMonth.Size = new System.Drawing.Size(159, 28);
+            this.comboMonth.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 661);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "myDataViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabTemperature.ResumeLayout(false);
+            this.tabHumidity.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartTemperature)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartHumidity)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabTemperature;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartTemperature;
+        private System.Windows.Forms.TabPage tabHumidity;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartHumidity;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.CheckedListBox checkedListSensors;
+        private System.Windows.Forms.ComboBox comboMonth;
+        private System.Windows.Forms.ComboBox comboYear;
+    }
+}
+
---
+++ myDataViewer/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace myDataViewer
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] Sensors =
+        {
+            "Outside", "Sanctuary", "Thermostat", "Centre", "Gallery"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
+
+            string dataRoot = Path.Combine(Application.StartupPath, "Data");
+
+            if (Directory.Exists(dataRoot))
+            {
+                var years = Directory.GetDirectories(dataRoot)
+                    .Select(Path.GetFileName)
+                    .OrderBy(y => y);
+
+                comboYear.Items.AddRange(years.ToArray());
+            }
+
+            checkedListSensors.Items.AddRange(Sensors);
+            checkedListSensors.CheckOnClick = true;
+
+            chartTemperature.ChartAreas.Add(new ChartArea("TempArea"));
+            chartHumidity.ChartAreas.Add(new ChartArea("HumArea"));
+
+            chartTemperature.Legends.Add(new Legend("TempLegend"));
+            chartHumidity.Legends.Add(new Legend("HumLegend"));
+
+            comboYear.SelectedIndex = 0;
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            if (comboYear.SelectedItem == null || comboMonth.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a year and month.");
+                return;
+            }
+
+            string basePath = Path.Combine(Application.StartupPath, "Data", comboYear.Text, comboMonth.Text);
+
+            chartTemperature.Series.Clear();
+            chartHumidity.Series.Clear();
+
+            foreach (string sensor in checkedListSensors.CheckedItems)
+            {
+                string csvPath = Path.Combine(basePath, sensor + ".csv");
+
+                if (!File.Exists(csvPath))
+                    continue;
+
+                var tempSeries = new Series(sensor)
+                {
+                    ChartType = SeriesChartType.Line,
+                    XValueType = ChartValueType.DateTime
+                };
+
+                var humSeries = new Series(sensor)
+                {
+                    ChartType = SeriesChartType.Line,
+                    XValueType = ChartValueType.DateTime
+                };
+
+                LoadCsvIntoSeries(csvPath, tempSeries, humSeries);
+
+                chartTemperature.Series.Add(tempSeries);
+                chartHumidity.Series.Add(humSeries);
+
+                //Add date time to graph. May get too busy so we comment out
+                //chartTemperature.ChartAreas[0].AxisX.LabelStyle.Format = "dd/MM HH:mm";
+                //chartHumidity.ChartAreas[0].AxisX.LabelStyle.Format = "dd/MM HH:mm";
+               
+                // As there are lots of days in a month angle the text on x-axis to show it all
+                chartTemperature.ChartAreas[0].AxisX.LabelStyle.Angle = -45;
+                chartTemperature.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+                chartTemperature.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+
+                chartHumidity.ChartAreas[0].AxisX.LabelStyle.Angle = -45;
+                chartHumidity.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+                chartHumidity.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+
+            }
+        }
+
+
+        //private void LoadCsvIntoSeries(string filePath, Series tempSeries, Series humSeries)
+        //{
+        //    foreach (var line in File.ReadLines(filePath).Skip(1))
+        //    {
+        //        var parts = line.Split(',');
+        //        if (parts.Length < 4)
+        //            continue;
+
+        //        string dateStr = parts[0];
+        //        string timeStr = parts[1];
+
+
+        //        if (!DateTime.TryParse($"{dateStr} {timeStr}", out DateTime timestamp))
+        //            continue;
+
+        //        if (double.TryParse(parts[2], NumberStyles.Any, CultureInfo.InvariantCulture, out double temp))
+        //        {
+        //            tempSeries.Points.AddXY(timestamp, temp);
+        //        }
+
+        //        if (double.TryParse(parts[3], NumberStyles.Any, CultureInfo.InvariantCulture, out double hum))
+        //        {
+        //            humSeries.Points.AddXY(timestamp, hum);
+        //        }
+        //    }
+        //}
+
+        private void LoadCsvIntoSeries(string filePath, Series tempSeries, Series humSeries)
+        {
+            foreach (var line in File.ReadLines(filePath).Skip(1))
+            {
+                var parts = line.Split(',');
+                if (parts.Length < 4)
+                    continue;
+
+                string combined = parts[0] + " " + parts[1];
+
+                //here we define how the date and time appears in the csv file
+                if (!DateTime.TryParseExact(
+                        combined,
+                        "MM/dd/yyyy HH:mm:ss",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out DateTime timestamp))
+                {
+                    continue;
+                }
+
+                if (double.TryParse(parts[2], NumberStyles.Any, CultureInfo.InvariantCulture, out double temp))
+                    tempSeries.Points.AddXY(timestamp, temp);
+
+                if (double.TryParse(parts[3], NumberStyles.Any, CultureInfo.InvariantCulture, out double hum))
+                    humSeries.Points.AddXY(timestamp, hum);
+            }
+        }
+
+
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void comboYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboMonth.Items.Clear();
+
+            string yearPath = Path.Combine(Application.StartupPath, "Data", comboYear.Text);
+
+            if (Directory.Exists(yearPath))
+            {
+                var months = Directory.GetDirectories(yearPath)
+                    .Select(Path.GetFileName)
+                    .OrderBy(m => m);
+
+                comboMonth.Items.AddRange(months.ToArray());
+
+                comboMonth.SelectedIndex = 0;
+            }
+        }
+
+    }
+}
